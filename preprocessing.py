0725302741aa0d59ca198750e4e8631a460c644a
Karolina Źróbek <karolina.m.zrobek@gmail.com>
2024-11-15 13:25:24 +0100

    Preprocess eeg to have only 20 timepoints

--- preprocessing.py
+++ preprocessing.py
@@ -11,7 +11,13 @@ This script prepares data for model training.
     This effectively standardizes the EEG signal segment, 
     making it centered around zero with a standard deviation of one
 
-2) It creates list of imagefiles
+2) Gets only 20 eeg time points from stimulus onset to 200ms after stimulus
+    And averages data of an image across 4 repetitions of the same image
+   (!) Overrites the original dataset with 100 time points per image
+    original data shape (16540 x 4 x 17 x 100)
+    preprocessed data shape (16540 x 17 x 20)
+   
+3) It creates list of imagefiles
     preprocessed_eeg_training[0,:,:,:] will correspond to the image /00001_aardvark/aardvark_01b.jpg, 
     preprocessed_eeg_training[1,:,:,:] will correspond to the image /00001_aardvark/aardvark_02s.jpg 
     etc...
@@ -26,22 +32,52 @@ img_train_dir = 'Images/training_images'
 img_test_dir = 'Images/test_images'
 
 def eeg_prep():
-    # EEG mean and std for each subject
-    preprocessed_mean_overall = np.array(10) # 10 subjects
-    preprocessed_std_overall = np.array(10)
+    # EEG mean and std for each subject, for every eeg channel 
+    preprocessed_mean_overall = [] #2d array (10,17) subjects x eeg chanels
+    preprocessed_std_overall = []
 
-    for i in range(1):
+    for i in range(10):
+        print(f'\tProcessing EEG data from subject {i+1}...')
         subject_dir = f'sub-{str(i+1).zfill(2)}'
-        data_path = os.path.join(eeg_dir, subject_dir, 'preprocessed_eeg_training.npy')
+        train_data_path = os.path.join(eeg_dir, subject_dir, 'preprocessed_eeg_training.npy')
+        test_data_path = os.path.join(eeg_dir, subject_dir, 'preprocessed_eeg_test.npy')
+
+        eeg_data_train = np.load(train_data_path, allow_pickle=True).tolist()['preprocessed_eeg_data']
+        eeg_data_test = np.load(test_data_path, allow_pickle=True).tolist()['preprocessed_eeg_data']
+
+        ## Resampling eeg
+        print(eeg_data_test.shape)
+        # Take only 200ms after stimulus onset
+        #  time steps are every 10ms, and there is 20 steps before stimulus onset
+        eeg_data_train = eeg_data_train[:,:,:,20:40] 
+        eeg_data_test = eeg_data_test[:,:,:,20:40]
+        print(eeg_data_test.shape)
+
+        # Average acorss image repetitions
+        eeg_data_train = np.mean(eeg_data_train, 1)
+        eeg_data_test = np.mean(eeg_data_test, 1)
+        print(eeg_data_test.shape)
+
+        print('Saving...')
+        #Overrites original data
+        np.save(train_data_path, eeg_data_train)
+        np.save(test_data_path, eeg_data_test)
+
+
+        ## Std and Mean across images
+        mean_train = np.mean(eeg_data_train, 0)
+        mean_test = np.mean(eeg_data_test, 0)
+        mean_all = (mean_train + mean_test)/ 2  
+
+        std_train = np.std(eeg_data_train, 0)
+        std_test = np.std(eeg_data_test, 0)
+        std_all = (std_train + std_test) / 2
         
-        eeg_data_train = np.load('preprocessed_eeg_data/sub-01/preprocessed_eeg_training.npy', allow_pickle=True)
-        
-        mean = np.mean(eeg_data_train.tolist()['preprocessed_eeg_data'])
-        std = np.std(eeg_data_train.tolist()['preprocessed_eeg_data'])
-        
-        preprocessed_mean_overall = np.append(preprocessed_mean_overall, mean)
-        preprocessed_std_overall= np.append(preprocessed_std_overall, std)
-        print(f'\tEEG ata from subject {i+1} processed')
+        preprocessed_mean_overall.append(mean_all)
+        preprocessed_std_overall.append(std_all)
+
+
+        print(f'\tEEG data from subject {i+1} processed.')
 
     print('Saving...')
     np.save(os.path.join(get_data_dir,'preprocessed_mean_overall.npy'), preprocessed_mean_overall)
@@ -80,9 +116,9 @@ if __name__ == '__main__':
     print("SUCCESS")
     
 
-    print("IMAGE PREP")
-    img_prep()
-    print("SUCCESS")
+    # print("IMAGE PREP")
+    # img_prep()
+    # print("SUCCESS")
    
     
 
